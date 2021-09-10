7586b004999776359db9acdda67ec4c948ad2fb1
Calvin <calvincharlesmead@gmail.com>
2021-09-10 13:07:31 -0500

    docs(g): g

--- src/frontend/src/views/About.vue
+++ src/frontend/src/views/About.vue
@@ -1,27 +1,30 @@
 <template>
   <div class="p-8">
-    <p class="text-bold font-bold pb-4">Meeting Efficiency Calculator (MEC)</p>
+    <p class="text-bold font-bold pb-4">Software Factory Metrics</p>
     <p>
-      This web application calculates the cost of meetings based on a number of
-      inputs. Upon saving the meeting, it stores that information in a SQL
-      database. The goal of this application is not to remove meetings, but to
-      put into perspective how much our meetings actually cost.
+      This web application provides a way for Rolls-Royce Software Factory to
+      track and display it's DORA metrics in accordance with modern DevSecOps
+      best practices. This is a requirement of the DoD and key in our digital
+      transformation.
     </p>
     <br />
-    <p>
-      This is also a proof of concept for deploying applications into the
-      Rolls-Royce Gov cloud. This is the first application deployed to the cloud
-      in Rolls-Royce, using modern software development.
-    </p>
+    <a
+      href="https://www.devops-research.com/research.html"
+      style="text-decoration: underline"
+    >
+      DORA Metrics Explained</a
+    >
+    <br />
+    <p></p>
     <br />
     <blockquote></blockquote>
     <blockquote
       className="relative p-4 text-xl italic border-l-4 bg-rrgrey-200 text-rrgrey-800 border-rrgrey-400 quote"
     >
       <p className="mb-4">
-        Ex: If you have a meeting with 20 people for 2 hours how much does that
-        cost your company? <br /><br />With the valuation of each employee being
-        $140/hr, that meeting will cost 20 * 2hr * 140 = <strong>$5600</strong>.
+        DORA is a research program providing an independent view into practices
+        and capabilities that drive high performance in technology delivery. It
+        is also used to compare against industry peers.
       </p>
     </blockquote>
   </div>
