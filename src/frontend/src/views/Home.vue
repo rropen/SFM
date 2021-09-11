b7b6b2724774bc4f8ccceb45c7d6dd2e911f8aab
Josh Haines <Josh@JoshHaines.com>
2021-09-11 07:14:58 -0400

    style(sidebar): add basic sidebar home layout

--- src/frontend/src/views/Home.vue
+++ src/frontend/src/views/Home.vue
@@ -26,9 +26,7 @@
           leave-from="translate-x-0"
           leave-to="-translate-x-full"
         >
-          <div
-            class="relative flex-1 flex flex-col max-w-xs w-full bg-gray-800"
-          >
+          <div class="relative flex-1 flex flex-col max-w-xs w-full bg-white">
             <TransitionChild
               as="template"
               enter="ease-in-out duration-300"
@@ -62,13 +60,6 @@
               </div>
             </TransitionChild>
             <div class="flex-1 h-0 pt-5 pb-4 overflow-y-auto">
-              <div class="flex-shrink-0 flex items-center px-4">
-                <img
-                  class="h-8 w-auto"
-                  src="https://tailwindui.com/img/logos/workflow-logo-indigo-500-mark-white-text.svg"
-                  alt="Workflow"
-                />
-              </div>
               <nav class="mt-5 px-2 space-y-1">
                 <a
                   v-for="item in navigation"
@@ -76,50 +67,24 @@
                   :href="item.href"
                   :class="[
                     item.current
-                      ? 'bg-gray-900 text-white'
-                      : 'text-gray-300 hover:bg-gray-700 hover:text-white',
-                    'group flex items-center px-2 py-2 text-base font-medium rounded-md',
+                      ? 'bg-white text-rrgrey-800'
+                      : 'text-rrgrey-600 ',
+                    'group flex items-center px-2 py-2 text-sm font-medium rounded-md',
                   ]"
                 >
                   <component
                     :is="item.icon"
                     :class="[
                       item.current
-                        ? 'text-gray-300'
-                        : 'text-gray-400 group-hover:text-gray-300',
-                      'mr-4 flex-shrink-0 h-6 w-6',
+                        ? 'text-rrgrey-800'
+                        : 'text-rrgrey-600 group-hover:text-rrgrey-600',
+                      'mr-3 flex-shrink-0 h-6 w-6',
                     ]"
                     aria-hidden="true"
                   />
                   {{ item.name }}
                 </a>
               </nav>
-            </div>
-            <div class="flex-shrink-0 flex bg-gray-700 p-4">
-              <a href="#" class="flex-shrink-0 group block">
-                <div class="flex items-center">
-                  <div>
-                    <img
-                      class="inline-block h-10 w-10 rounded-full"
-                      src="https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80"
-                      alt=""
-                    />
-                  </div>
-                  <div class="ml-3">
-                    <p class="text-base font-medium text-white">Tom Cook</p>
-                    <p
-                      class="
-                        text-sm
-                        font-medium
-                        text-gray-400
-                        group-hover:text-gray-300
-                      "
-                    >
-                      View profile
-                    </p>
-                  </div>
-                </div>
-              </a>
             </div>
           </div>
         </TransitionChild>
@@ -133,24 +98,17 @@
     <div class="hidden md:flex md:flex-shrink-0">
       <div class="flex flex-col w-64">
         <!-- Sidebar component, swap this element with another sidebar if you like -->
-        <div class="flex-1 flex flex-col min-h-0 bg-gray-800">
+        <div class="flex-1 flex flex-col min-h-0 bg-white">
           <div class="flex-1 flex flex-col pt-5 pb-4 overflow-y-auto">
-            <div class="flex items-center flex-shrink-0 px-4">
-              <img
-                class="h-8 w-auto"
-                src="https://tailwindui.com/img/logos/workflow-logo-indigo-500-mark-white-text.svg"
-                alt="Workflow"
-              />
-            </div>
-            <nav class="mt-5 flex-1 px-2 bg-gray-800 space-y-1">
+            <nav class="mt-5 flex-1 px-2 bg-white space-y-1">
               <a
                 v-for="item in navigation"
                 :key="item.name"
                 :href="item.href"
                 :class="[
                   item.current
-                    ? 'bg-gray-900 text-white'
-                    : 'text-gray-300 hover:bg-gray-700 hover:text-white',
+                    ? 'bg-white text-rrgrey-800'
+                    : 'text-rrgrey-600 ',
                   'group flex items-center px-2 py-2 text-sm font-medium rounded-md',
                 ]"
               >
@@ -158,8 +116,8 @@
                   :is="item.icon"
                   :class="[
                     item.current
-                      ? 'text-gray-300'
-                      : 'text-gray-400 group-hover:text-gray-300',
+                      ? 'text-rrgrey-800'
+                      : 'text-rrgrey-600 group-hover:text-rrgrey-600',
                     'mr-3 flex-shrink-0 h-6 w-6',
                   ]"
                   aria-hidden="true"
@@ -167,32 +125,6 @@
                 {{ item.name }}
               </a>
             </nav>
-          </div>
-          <div class="flex-shrink-0 flex bg-gray-700 p-4">
-            <a href="#" class="flex-shrink-0 w-full group block">
-              <div class="flex items-center">
-                <div>
-                  <img
-                    class="inline-block h-9 w-9 rounded-full"
-                    src="https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80"
-                    alt=""
-                  />
-                </div>
-                <div class="ml-3">
-                  <p class="text-sm font-medium text-white">Tom Cook</p>
-                  <p
-                    class="
-                      text-xs
-                      font-medium
-                      text-gray-300
-                      group-hover:text-gray-200
-                    "
-                  >
-                    View profile
-                  </p>
-                </div>
-              </div>
-            </a>
           </div>
         </div>
       </div>
@@ -223,10 +155,19 @@
           <MenuIcon class="h-6 w-6" aria-hidden="true" />
         </button>
       </div>
-      <main class="flex-1 relative z-0 overflow-y-auto focus:outline-none">
+      <main
+        class="
+          flex-1
+          relative
+          z-0
+          overflow-y-auto
+          focus:outline-none
+          bg-rrgrey-200
+        "
+      >
         <div class="py-6">
           <div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
-            <h1 class="text-2xl font-semibold text-gray-900">Dashboard</h1>
+            <h1 class="text-2xl font-semibold text-rrgry-900">Content</h1>
           </div>
           <div class="max-w-7xl mx-auto px-4 sm:px-6 md:px-8">
             <!-- Replace with your content -->
