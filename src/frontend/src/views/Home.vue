0a084fca511824c434b1ae9e88b2ae4b2bb256c4
William Belcher <willcbelcher@gmail.com>
2021-09-15 10:09:15 -0400

    fix(home view): update padding styling of Home page
    #30

--- src/frontend/src/views/Home.vue
+++ src/frontend/src/views/Home.vue
@@ -211,10 +211,10 @@
         "
       >
         <div class="py-6">
-          <div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
+          <div class="max-w-full mx-auto px-4 sm:px-6 lg:px-14">
             <h1 class="text-2xl font-semibold text-rrgrey-900">Project Name</h1>
           </div>
-          <div class="max-w-7xl mx-auto px-4 sm:px-6 md:px-8">
+          <div class="max-w-full mx-auto px-4 sm:px-6 md:px-14">
             <!-- Replace with your content -->
             <div class="py-4">
               <div
