7b6b04e2e78fcc2813bc42de084ca8a1e12679e8
Tyler Blaszak <tab890@yahoo.com>
2021-09-16 13:00:11 -0400

    feat(chartsAPI): created test endpoint which returns deployment dates as unix

--- src/backend/sfm/routes/charts/routes.py
+++ src/backend/sfm/routes/charts/routes.py
@@ -1,9 +1,10 @@
 from datetime import datetime, timedelta
+from time import mktime
 from dateutil.relativedelta import relativedelta
 from statistics import median
 from sfm.routes.work_items import crud
 from sfm.routes.projects import crud as proj_crud
-from sfm.models import WorkItem, Project, ChartData
+from sfm.models import WorkItem, Project, ChartData, WorkItemCategory
 from typing import List, Optional
 from sqlmodel import Session, select, and_
 from fastapi import APIRouter, HTTPException, Depends, Path, Header, Request
@@ -110,7 +111,7 @@ def calc_frequency(
 
 @router.get("/", response_model=List[ChartData])
 def get_deployments(
-    category: str,
+    category: WorkItemCategory,
     project_id: Optional[int] = None,
     project_name: Optional[str] = None,
     group_projects: Optional[bool] = False,
@@ -188,5 +189,98 @@ def get_deployments(
         ]
     else:
         chart_data = group_deployments
+
+    return chart_data
+
+
+@router.get("/test/", response_model=List[ChartData])
+def get_deployments_test(
+    category: WorkItemCategory,
+    project_id: Optional[int] = None,
+    project_name: Optional[str] = None,
+    group_projects: Optional[bool] = False,
+    db: Session = Depends(get_db),
+):
+    """
+    ## Get Chart Info (Dates return in UNIX time, naive format)
+
+    Get the json data needed to produce an SFM chart on frontend
+    """
+    project = None
+    if project_name and not project_id:
+        project = db.exec(select(Project).where(Project.name == project_name)).first()
+        if not project:
+            return False
+    elif project_id and not project_name:
+        project = db.get(Project, project_id)
+        if not project:
+            return False
+    elif project_id and project_name:
+        project = db.exec(
+            select(Project).where(
+                and_(Project.id == project_id, Project.name == project_name)
+            )
+        ).first()
+        if not project:
+            return False
+
+    if project:
+        # return specific project deployment frequency json object
+        project_name = project.name
+        deployments = [
+            item for item in project.work_items if (item.category == "Deployment")
+        ]
+        deployment_dates = [
+            mktime(deploy.end_time.date().timetuple()) for deploy in deployments
+        ]
+        deploy_frequency = calc_frequency(deployments)
+
+    elif group_projects:
+        print("Test in loop")
+        projects = proj_crud.get_all(db)
+        group_deployments = []
+        for project in projects:
+            project_name = project.name
+            deployments = []
+            deployment_dates = []
+            for work_item in project.work_items:
+                if work_item.category == "Deployment":
+                    deployments.append(work_item)
+                    deployment_dates.append(
+                        mktime(work_item.end_time.date().timetuple())
+                    )
+
+            deploy_frequency = calc_frequency(deployments)
+            group_deployments.append(
+                {
+                    "project_name": project_name,
+                    "deployment_dates": deployment_dates,
+                    "deployment_frequency": deploy_frequency,
+                }
+            )
+
+    else:
+        all_items = crud.get_all(db)
+        deployments = [item for item in all_items if (item.category == "Deployment")]
+        project_name = "org"
+        deployment_dates = [
+            mktime(deploy.end_time.date().timetuple()) for deploy in deployments
+        ]
+        deploy_frequency = calc_frequency(deployments)
+
+    if not group_projects:
+        chart_data = [
+            {
+                "project_name": project_name,
+                "deployment_dates": deployment_dates,
+                "deployment_frequency": deploy_frequency,
+            }
+        ]
+    else:
+        chart_data = group_deployments
+
+    for obj in chart_data:
+        for date in obj["deployment_dates"]:
+            print(datetime.utcfromtimestamp(int(date)).strftime("%Y-%m-%d %H:%M:%S"))
 
     return chart_data
