a7aa77e5e5a3b5eec05d1c5e99ec085dde976383
Tyler Blaszak <tab890@yahoo.com>
2021-09-16 11:01:43 -0400

    feat(charts): add project group to response
    Charts endpoint can now group deployments by project.
    
    BREAKING CHANGE: Charts api endpoint now returns a list of objects instead of a single object. The
    objects now include a project_name field with the name of the project. If returning all deployments,
    this project_name is 'org'.
    
    #24

--- src/backend/sfm/routes/charts/routes.py
+++ src/backend/sfm/routes/charts/routes.py
@@ -2,6 +2,7 @@ from datetime import datetime, timedelta
 from dateutil.relativedelta import relativedelta
 from statistics import median
 from sfm.routes.work_items import crud
+from sfm.routes.projects import crud as proj_crud
 from sfm.models import WorkItem, Project, ChartData
 from typing import List, Optional
 from sqlmodel import Session, select, and_
@@ -18,11 +19,9 @@ def get_db():
 router = APIRouter()
 
 
-def calc_frequency(deployments: List, deployment_dates: List):
-
-    # print(deployment_dates)
-    # ^^^ We will return the dates of this back to the front end to display on charts. We need to only grab
-    #     the last three months of deployments for our "Current Status" metric
+def calc_frequency(
+    deployments: List,
+):  # generates the DORA metric based on only deployments in the last 3 months
     three_months_ago = datetime.now() - timedelta(days=84)
     recent_deploy_dates = [
         deployment.end_time
@@ -60,9 +59,8 @@ def calc_frequency(deployments: List, deployment_dates: List):
 
         week_start += timedelta(days=7)
 
-    monthly_deploys = (
-        []
-    )  # list of 3 integers that represent if a deploy happened during that month
+    monthly_deploys = []
+    # list of 3 integers that represent if a deploy happened during that month
     # for i, week in enumerate(weekly_deployed):
     #     monthly_deploys[i % 3] += week
 
@@ -110,11 +108,12 @@ def calc_frequency(deployments: List, deployment_dates: List):
     # jsonData.append({"week-range": deployFreq})
 
 
-@router.get("/", response_model=ChartData)
-def get_work_items(
+@router.get("/", response_model=List[ChartData])
+def get_deployments(
     category: str,
     project_id: Optional[int] = None,
     project_name: Optional[str] = None,
+    group_projects: Optional[bool] = False,
     db: Session = Depends(get_db),
 ):
     """
@@ -147,20 +146,46 @@ def get_work_items(
             item for item in project.work_items if (item.category == "Deployment")
         ]
         deployment_dates = [deploy.end_time.date() for deploy in deployments]
+        deploy_frequency = calc_frequency(deployments)
 
-        deploy_frequency = calc_frequency(deployments, deployment_dates)
+    elif group_projects:
+        print("Test in loop")
+        projects = proj_crud.get_all(db)
+        group_deployments = []
+        for project in projects:
+            project_name = project.name
+            deployments = []
+            deployment_dates = []
+            for work_item in project.work_items:
+                if work_item.category == "Deployment":
+                    deployments.append(work_item)
+                    deployment_dates.append(work_item.end_time.date())
+
+            deploy_frequency = calc_frequency(deployments)
+            group_deployments.append(
+                {
+                    "project_name": project_name,
+                    "deployment_dates": deployment_dates,
+                    "deployment_frequency": deploy_frequency,
+                }
+            )
 
     else:
-        # return all project deployment frequency data in json object
         all_items = crud.get_all(db)
         deployments = [item for item in all_items if (item.category == "Deployment")]
+        project_name = "org"
         deployment_dates = [deploy.end_time.date() for deploy in deployments]
+        deploy_frequency = calc_frequency(deployments)
 
-        deploy_frequency = calc_frequency(deployments, deployment_dates)
+    if not group_projects:
+        chart_data = [
+            {
+                "project_name": project_name,
+                "deployment_dates": deployment_dates,
+                "deployment_frequency": deploy_frequency,
+            }
+        ]
+    else:
+        chart_data = group_deployments
 
-    chart_dict = {
-        "deployment_dates": deployment_dates,
-        "deployment_frequency": deploy_frequency,
-    }
-
-    return chart_dict
+    return chart_data
