8d7253130e74355c9492dd0ffb367a4ea72e4eb5
Gabe Geiger <gabe.geiger@gmail.com>
2021-09-21 16:38:08 -0400

    fix(conftest): adjustments to test database creation

--- src/backend/tests/conftest.py
+++ src/backend/tests/conftest.py
@@ -3,6 +3,7 @@ import pytest
 from sqlmodel import SQLModel, create_engine, Session
 from starlette.testclient import TestClient
 from sqlalchemy.engine import Engine as Database
+from sqlalchemy_utils import database_exists, drop_database, create_database
 
 from sfm.main import app
 from sfm.routes.utilities import routes as utilities
@@ -28,14 +29,34 @@ def get_test_db() -> AsyncIterable[Session]:
         sess.close()
 
 
-@pytest.fixture(scope="session", autouse=True)
+@pytest.fixture(scope="module", autouse=True)
 def setup_database():
+
+    if database_exists(DATABASE_URL):
+        drop_database(DATABASE_URL)
 
     SQLModel.metadata.create_all(engine)
 
-    # client = TestClient(app)
-    # client.post("/utilities/populate_mock_data")
-    # utilities.populate_db(Session(engine))
+    app.dependency_overrides[utilities.get_db] = get_test_db
+
+    client = TestClient(app)
+    client.post("/utilities/populate_mock_data")
+
+    drop_database(DATABASE_URL)
+
+
+@pytest.yield_fixture
+def test_db_session():
+    """Returns an sqlalchemy session, and after the test tears down everything properly."""
+
+    session = Session(bind=engine)
+
+    yield session
+    # Drop all data after each test
+    for tbl in reversed(SQLModel.metadata.sorted_tables):
+        engine.execute(tbl.delete())
+    # put back the connection to the connection pool
+    session.close()
 
 
 # Creates a new connection
--- src/backend/tests/work_items/test_work_items.py
+++ src/backend/tests/work_items/test_work_items.py
@@ -1,10 +1,22 @@
 from fastapi.testclient import TestClient
 from sqlmodel import Session
-from tests.conftest import engine
+from tests.conftest import engine, test_db_session as db
 from sfm.routes.work_items import crud, routes
+from sfm.routes.projects import crud as proj_crud
 
+from sfm.models import WorkItemCreate
 
-def test_get_all(test_app):
-    response = crud.get_all(Session(engine), skip=0, limit=10)
+"""
+def test_get_all(test_app, db):
+    response = crud.get_all(db, skip=0, limit=10)
     print(response)
     assert len(response) == 10
+
+
+def test_create(test_app, db):
+    work_item_data = WorkItemCreate(**{"category": "Deployment", "project_id": 1})
+    response = crud.create_work_item(
+        db, work_item_data, project_auth_token="l~$~Xv111j1O2H$_&0uo"
+    )
+    assert isinstance(response, int)
+"""
