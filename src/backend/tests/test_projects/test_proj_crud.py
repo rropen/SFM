b54e126aa267790e5ffc403a4afee6fb0232d17e
Tyler Blaszak <tab890@yahoo.com>
2021-09-23 09:33:44 -0400

    feat(backend testing): added example project test file structure

---
+++ src/backend/tests/test_projects/test_proj_crud.py
@@ -0,0 +1,19 @@
+import pytest
+
+
+# get_all
+
+
+# get_by_id
+
+
+# create_project
+
+
+# delete_project
+
+
+# refresh_project_key
+
+
+# update_project
---
+++ src/backend/tests/test_projects/test_proj_routes.py
@@ -0,0 +1,19 @@
+import pytest
+
+
+# get at "/"
+
+
+# get at "/{project_id}"
+
+
+# post at "/"
+
+
+# delete at "/{project_id}"
+
+
+# post at "/{project_id}"
+
+
+# patch at "/{project_id}"
