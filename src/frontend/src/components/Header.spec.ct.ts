dbaca0abe774d47bedb663ffb99353ce2d1a311a
Josh Haines <Josh@JoshHaines.com>
2021-09-29 12:05:04 -0400

    test(header): fix header test

--- src/frontend/src/components/Header.spec.ct.ts
+++ src/frontend/src/components/Header.spec.ct.ts
@@ -1,8 +1,10 @@
+/// <reference types="cypress" />
 import { mount } from "@cypress/vue";
 import Header from "./Header.vue";
 import "../index.css";
 
 beforeEach(() => {
+  //@ts-ignore
   mount(Header);
 });
 
