3614a116360911aa92bf6783ed82ca0d30e5e5e7
Ian Mungovan <ian.mungovan1@gmail.com>
2021-09-30 15:19:04 -0400

    feat(multiple files): backend Connections String to Azure
    Implements connection setup for Azure

--- src/backend/sfm/database.py
+++ src/backend/sfm/database.py
@@ -1,22 +1,38 @@
 from sqlmodel import SQLModel, Session, create_engine
+
 import os
 import config
 import logging
+import pyodbc
+import urllib
 
-# This is the Database Connection for Azure
-# server = ''
-# database = ''
-# username = ''
-# password = ''
-# driver= '{ODBC Driver 17 for SQL Server}'
+# # SECRET_KEY = os.environ.get("SECRET_KEY") or "secret-key"
+# DBHOST = os.environ.get("DBHOST") or server
+# DBNAME = os.environ.get("DBNAME") or database
+# DBUSER = os.environ.get("DBUSER") or username
+# DBPASS = os.environ.get("DBPASS") or password
 
-# SECRET_KEY = os.environ.get('SECRET_KEY') or 'secret-key'
-# SQL_SERVER = os.environ.get('SQL_SERVER') or server
-# SQL_DATABASE = os.environ.get('SQL_DATABASE') or database
-# SQL_USER_NAME = os.environ.get('SQL_USER_NAME') or username
-# SQL_PASSWORD = os.environ.get('SQL_PASSWORD') or password
-# SQLALCHEMY_DATABASE_URI = 'mssql+pyodbc://' + SQL_USER_NAME + '@' + SQL_SERVER + ':' + SQL_PASSWORD + '@' + SQL_SERVER + ':1433/' + SQL_DATABASE + '?driver=ODBC+Driver+17+for+SQL+Server'
-# SQLALCHEMY_TRACK_MODIFICATIONS = False
+conn_str = "sqlite:///database.db"
+
+# driver = "{ODBC Driver 17 for SQL Server}"
+# server = ""
+# user = ""
+# database = ""
+# password = ""
+
+# conn = f"""Driver={driver};Server=tcp:{server},1433;Database={database};
+# Uid={user};Pwd={password};Encrypt=yes;TrustServerCertificate=no;Connection Timeout=30;"""
+
+# params = urllib.parse.quote_plus(conn)
+# conn_str = "mssql+pyodbc:///?autocommit=true&odbc_connect={}".format(params)
+
+# # check_same_thread = false only works in sqlite, not postgres or others
+if "sqlite" in conn_str:
+    print("Using a sqlite database")
+    connect_args = {"check_same_thread": False}
+    engine = create_engine(conn_str, connect_args=connect_args)
+else:
+    engine = create_engine(conn_str, echo=True)
 
 logging.basicConfig(
     filename="logs.log",
@@ -24,17 +40,6 @@ logging.basicConfig(
     format="%(levelname)s %(name)s %(asctime)s %(message)s",
 )
 logger = logging.getLogger(__name__)
-
-DATABASE_URL = config.DATABASE_URL
-assert DATABASE_URL != ""
-
-# check_same_thread = false only works in sqlite, not postgres or others
-if "sqlite" in DATABASE_URL:
-    print("Using a sqlite database")
-    connect_args = {"check_same_thread": False}
-    engine = create_engine(DATABASE_URL, connect_args=connect_args)
-else:
-    engine = create_engine(DATABASE_URL)
 
 
 def create_db_and_tables():
