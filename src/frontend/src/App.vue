b9cb6b991fd88610baac94233c79cb03dbcba811
William Belcher <willcbelcher@gmail.com>
2021-09-30 15:47:53 -0400

    feat(header): refactor to use rrHeader component
    #67

--- src/frontend/src/App.vue
+++ src/frontend/src/App.vue
@@ -1,7 +1,12 @@
 <template>
   <div class="bg-rrblue-800 min-h-full overflow-auto">
     <div id="app" class="h-full font-sans bg-rrgrey-200" style="top: 50%">
-      <Header />
+      <rrHeader
+        :appTitleShort="appTitleShort"
+        :appTitleLong="appTitleLong"
+        :tabsText="tabsText"
+        :tabLinks="tabLinks"
+      />
       <div class="page bg-rrgrey-200 h-full">
         <router-view />
       </div>
@@ -11,7 +16,7 @@
 </template>
 
 <script lang="ts" setup>
-import Header from "./components/Header.vue";
+import { rrHeader } from "@rrglobal/vue-cobalt";
 import { rrFooter } from "@rrglobal/vue-cobalt";
 import { computed } from "vue";
 import { useRoute } from "vue-router";
@@ -19,6 +24,13 @@ import { useRoute } from "vue-router";
 const appText =
   "SFM (Software Factory Metrics) is an app to present DevOps metrics for the various applications being built throughout the Software Factory.  These include metrics such as Deployment Frequency which can give insights into the health of the software delivery processes at work and business value delivery in the greater Software Value Stream.";
 const route = useRoute();
+
+const appTitleShort = "SFM";
+const appTitleLong = "Software Factory Metrics";
+const tabsText = ["DORA", "Flow", "About"];
+
+const tabLinks = ["Dora", "Flow", "About"];
+
 const pathName = computed(() => route.name);
 </script>
 <style></style>
