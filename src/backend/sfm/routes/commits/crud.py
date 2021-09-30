b90d69f5cf512f2e6a2cb0c766daec94290e411c
Gabe Geiger <gabe.geiger@gmail.com>
2021-09-30 16:19:48 -0400

    feat(commit_model): change primary key for commit table

--- src/backend/sfm/routes/commits/crud.py
+++ src/backend/sfm/routes/commits/crud.py
@@ -57,7 +57,7 @@ def get_all(
 
 def get_by_sha(db: Session, commit_sha):
     """Get a specified Commit and return it."""
-    return db.get(Commit, commit_sha)
+    return db.exec(select(Commit).where(Commit.sha == commit_sha)).first()
 
 
 def create_commit(db: Session, commit_data, project_auth_token):
@@ -94,7 +94,7 @@ def create_commit(db: Session, commit_data, project_auth_token):
 
 def delete_commit(db: Session, commit_sha, project_auth_token):
     """Take a commit and remove the row from the database."""
-    commit = db.get(Commit, commit_sha)
+    commit = db.exec(select(Commit).where(Commit.sha == commit_sha)).first()
     if not commit:
         logger.warning('func="delete_commit" warning="Item not found"')
         raise HTTPException(status_code=404, detail="Item not found")
@@ -113,7 +113,7 @@ def delete_commit(db: Session, commit_sha, project_auth_token):
         raise HTTPException(status_code=401, detail="Credentials are incorrect")
 
     # Check our work
-    row = db.get(Commit, commit_sha)
+    row = db.exec(select(Commit).where(Commit.sha == commit_sha)).first()
     if row:
         logger.error(
             'func="delete_commit" error="Item did not delete correctly and still exists"'
@@ -127,7 +127,7 @@ def delete_commit(db: Session, commit_sha, project_auth_token):
 
 def update_commit(db: Session, commit_sha, commit_data, project_auth_token):
     """Take data from request and update an existing Commit in the database."""
-    commit = db.get(Commit, commit_sha)
+    commit = db.exec(select(Commit).where(Commit.sha == commit_sha)).first()
     if not commit:
         logger.warning('func="update_commit" warning="Item not found"')
         raise HTTPException(status_code=404, detail="Item not found")
