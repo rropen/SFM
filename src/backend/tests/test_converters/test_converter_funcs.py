0bcbb3a7caf201e6b334b1ba9f16e96430c8770d
Tyler Blaszak <tab890@yahoo.com>
2021-10-04 16:46:34 -0400

    feat(github api): fix final testing of function

--- src/backend/tests/test_converters/test_converter_funcs.py
+++ src/backend/tests/test_converters/test_converter_funcs.py
@@ -11,19 +11,25 @@ import json
 import os
 
 
-# def test_github_backpopulate(client: TestClient,db: Session):
-#     """Clearing database so that id's are easier to test"""
-#     response = client.get("/converters/github_populate", params={"org": "rropen", "repo": "testing"})
-#     assert response.status_code == 200
+def test_github_backpopulate(client: TestClient, db: Session):
+    """Clearing database so that id's are easier to test"""
+    response = client.get(
+        "/converters/github_populate", params={"org": "rropen", "repo": "testing"}
+    )
+    assert response.status_code == 200
 
-#     project = db.exec(select(Project).where(Project.name == "testing")).first()
-#     assert project is not None
+    project = db.exec(select(Project).where(Project.name == "testing")).first()
+    assert project is not None
 
-#     pull_requests = db.exec(select(WorkItem).where(WorkItem.project_id == project.id)).all()
-#     assert len(pull_requests) == 1
+    pull_requests = db.exec(
+        select(WorkItem).where(WorkItem.project_id == project.id)
+    ).all()
+    assert len(pull_requests) == 1
 
-#     commits = db.exec(select(Commit).where(Commit.work_item_id == pull_requests[0].id)).all()
-#     assert len(commits) == 1
-#     assert commits[0].sha == "6c25310a034145701775e620895c7b36d16fc1c4"
-#     assert commits[0].work_item_id == 44
-#     assert commits[0].time_to_pull == timedelta()
+    commits = db.exec(
+        select(Commit).where(Commit.work_item_id == pull_requests[0].id)
+    ).all()
+    assert len(commits) == 1
+    assert commits[0].sha == "6c25310a034145701775e620895c7b36d16fc1c4"
+    assert commits[0].work_item_id == 50
+    assert commits[0].time_to_pull == 373
