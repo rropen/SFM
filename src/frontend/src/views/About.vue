6e5f675e7ea8b9314e35335d6d2d4b2ed13b411d
Calvin <calvincharlesmead@gmail.com>
2021-10-05 17:33:14 -0500

    feat(metric calcs): updated about page

--- src/frontend/src/views/About.vue
+++ src/frontend/src/views/About.vue
@@ -26,27 +26,59 @@
         and capabilities that drive high performance in technology delivery. It
         is also used to compare against industry peers.
       </p>
-      <p>
-        <b>Deployment Frequency:</b> How often we as an organization
-        successfully releases to production - Elite: Multiple deploys per day -
-        High: Between once per day and once per week - Medium: Between once per
-        week and once per month - Low: Between once per month and once every six
-        months **Lead
-        <br />
-        <b>Time for Changes:</b> The amount of time it takes a commit to get
-        into production - Elite: Less than one day - High: Between one day and
-        one week - Medium: Between one week and one month - Low: Between one
-        month and six months
-        <br />
-        <b>Time to Restore Service:</b> How long it takes an organization to
-        recover from a failure in production - Elite: Less than one hour - High:
-        Less than one day - Medium: Less than one day - Low: Between one week
-        and one month
-        <br />
-        <b>Change failure rate:</b> The percentage of deployments causing a
-        failure in production - Elite: 0-15% - High: 0-15% - Medium: 0-15% -
-        Low: 46-60%
-      </p>
+      <table class="table-auto">
+        <thead>
+          <tr>
+            <th>Software Delivery Metric</th>
+            <th>Elite</th>
+            <th>High</th>
+            <th>Medium</th>
+            <th>Low</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>
+              <b>Deployment Frequency:</b> How often we as an organization
+              successfully releases to production
+            </td>
+            <td>Multiple deploys per day</td>
+            <td>Between once per day and once per week</td>
+            <td>Between once per week and once per month</td>
+            <td>Between once per month and once every six months</td>
+          </tr>
+          <tr class="bg-emerald-200">
+            <td>
+              <b>Time for Changes:</b> The amount of time it takes a commit to
+              get into production
+            </td>
+            <td>Less than one day</td>
+            <td>Between one day and one week</td>
+            <td>Between one week and one month</td>
+            <td>Between one month and six months</td>
+          </tr>
+          <tr>
+            <td>
+              <b>Time to Restore Service:</b> How long it takes an organization
+              to recover from a failure in production
+            </td>
+            <td>Less than one hour</td>
+            <td>Less than one day</td>
+            <td>Less than one day</td>
+            <td>Between one week and one month</td>
+          </tr>
+          <tr>
+            <td>
+              <b>Change failure rate:</b> The percentage of deployments causing
+              a failure in production
+            </td>
+            <td>0-15%</td>
+            <td>0-15%</td>
+            <td>0-15%</td>
+            <td>46-60%</td>
+          </tr>
+        </tbody>
+      </table>
     </blockquote>
   </div>
 </template>
