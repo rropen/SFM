159dee05cbef8f140b62517bce56738dfdcec603
Gabe Geiger <gabe.geiger@gmail.com>
2021-10-07 15:17:08 -0400

    fix(deployment_config): add debugging for deployments

--- src/backend/sfm/config.py
+++ src/backend/sfm/config.py
@@ -64,7 +64,6 @@ class Settings(BaseSettings):
     DBNAME: str = os.environ.get("DBNAME") or "unset"
     DBUSER: str = os.environ.get("DBUSER") or "unset"
     DBPASS: str = os.environ.get("DBPASS") or "unset"
-    print(os.environ.get("AZURELOGGING_CONN_STR"))
     AZURE_LOGGING_CONN_STR: str = os.environ.get("AZURE_LOGGING_CONN_STR") or "unset"
     CONN_STR: str = generate_db_string(ENV, DBHOST, DBNAME, DBUSER, DBPASS)
 
