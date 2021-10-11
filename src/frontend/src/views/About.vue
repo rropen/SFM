2c0b1abcdc5dd0ffd71d954bf5cf1b87bd57daeb
Calvin <calvincharlesmead@gmail.com>
2021-10-11 07:19:27 -0500

    feat(sfm calcs): added boarders to table

--- src/frontend/src/views/About.vue
+++ src/frontend/src/views/About.vue
@@ -17,6 +17,68 @@
     <br />
     <p></p>
     <br />
+    <table class="border-separate border border-blue-800">
+      <thead>
+        <tr>
+          <th class="border border-blue-600">Software Delivery Metric</th>
+          <th class="border border-blue-600">Elite</th>
+          <th class="border border-blue-600">High</th>
+          <th class="border border-blue-600">Medium</th>
+          <th class="border border-blue-600">Low</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr>
+          <td class="border border-blue-600">
+            <b>Deployment Frequency:</b> How often we as an organization
+            successfully releases to production
+          </td>
+          <td class="border border-blue-600">Multiple deploys per day</td>
+          <td class="border border-blue-600">
+            Between once per day and once per week
+          </td>
+          <td class="border border-blue-600">
+            Between once per week and once per month
+          </td>
+          <td class="border border-blue-600">
+            Between once per month and once every six months
+          </td>
+        </tr>
+        <tr class="bg-emerald-200">
+          <td class="border border-blue-600">
+            <b>Time for Changes:</b> The amount of time it takes a commit to get
+            into production
+          </td>
+          <td class="border border-blue-600">Less than one day</td>
+          <td class="border border-blue-600">Between one day and one week</td>
+          <td class="border border-blue-600">Between one week and one month</td>
+          <td class="border border-blue-600">
+            Between one month and six months
+          </td>
+        </tr>
+        <tr>
+          <td class="border border-blue-600">
+            <b>Time to Restore Service:</b> How long it takes an organization to
+            recover from a failure in production
+          </td>
+          <td class="border border-blue-600">Less than one hour</td>
+          <td class="border border-blue-600">Less than one day</td>
+          <td class="border border-blue-600">Less than one day</td>
+          <td class="border border-blue-600">Between one week and one month</td>
+        </tr>
+        <tr>
+          <td class="border border-blue-600">
+            <b>Change failure rate:</b> The percentage of deployments causing a
+            failure in production
+          </td>
+          <td class="border border-blue-600">0-15%</td>
+          <td class="border border-blue-600">0-15%</td>
+          <td class="border border-blue-600">0-15%</td>
+          <td class="border border-blue-600">46-60%</td>
+        </tr>
+      </tbody>
+    </table>
+    <br />
     <blockquote></blockquote>
     <blockquote
       className="relative p-4 text-xl italic border-l-4 bg-rrgrey-200 text-rrgrey-800 border-rrgrey-400 quote"
@@ -26,59 +88,6 @@
         and capabilities that drive high performance in technology delivery. It
         is also used to compare against industry peers.
       </p>
-      <table class="table-auto">
-        <thead>
-          <tr>
-            <th>Software Delivery Metric</th>
-            <th>Elite</th>
-            <th>High</th>
-            <th>Medium</th>
-            <th>Low</th>
-          </tr>
-        </thead>
-        <tbody>
-          <tr>
-            <td>
-              <b>Deployment Frequency:</b> How often we as an organization
-              successfully releases to production
-            </td>
-            <td>Multiple deploys per day</td>
-            <td>Between once per day and once per week</td>
-            <td>Between once per week and once per month</td>
-            <td>Between once per month and once every six months</td>
-          </tr>
-          <tr class="bg-emerald-200">
-            <td>
-              <b>Time for Changes:</b> The amount of time it takes a commit to
-              get into production
-            </td>
-            <td>Less than one day</td>
-            <td>Between one day and one week</td>
-            <td>Between one week and one month</td>
-            <td>Between one month and six months</td>
-          </tr>
-          <tr>
-            <td>
-              <b>Time to Restore Service:</b> How long it takes an organization
-              to recover from a failure in production
-            </td>
-            <td>Less than one hour</td>
-            <td>Less than one day</td>
-            <td>Less than one day</td>
-            <td>Between one week and one month</td>
-          </tr>
-          <tr>
-            <td>
-              <b>Change failure rate:</b> The percentage of deployments causing
-              a failure in production
-            </td>
-            <td>0-15%</td>
-            <td>0-15%</td>
-            <td>0-15%</td>
-            <td>46-60%</td>
-          </tr>
-        </tbody>
-      </table>
     </blockquote>
   </div>
 </template>
