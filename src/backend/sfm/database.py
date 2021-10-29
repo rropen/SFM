3ea229e8872c0092c7824f0e44f3bab2feb1e8f0
Ian Mungovan <ian.mungovan1@gmail.com>
2021-10-29 14:05:27 -0400

    test(frontend.spec.ts): fixed Integration Tests
    Fixed Integration tests

--- src/backend/sfm/database.py
+++ src/backend/sfm/database.py
@@ -20,23 +20,13 @@ def generate_db_string(ENV: str, DBHOST: str, DBNAME: str, DBUSER: str, DBPASS: 
             raise ValueError(
                 "Missing database parameter in the environment.  Please specify DBHOST, DBNAME, DBUSER, and DBPASS"
             )
-
-        # driver = "{ODBC Driver 17 for SQL Server}"
-        # conn = f"""dbname='{DBNAME}' user='sfadmin@psql-sfm' host='psql-sfm.postgres.database.usgovcloudapi.net'
-        # password='{DBPASS}' port='5432' sslmode='true'"""
         conn = "host={0} user={1} dbname={2} password={3} sslmode={4}".format(
             DBHOST, DBUSER, DBNAME, DBPASS, "require"
         )
         conn = f"postgresql+psycopg2://{DBUSER}:{DBPASS}@{DBHOST}/{DBNAME}"
 
-        # conn = f"""Driver={driver};Server=tcp:{DBHOST},1433;Database={DBNAME};
-        # Uid={DBUSER};Pwd={DBPASS};Encrypt=yes;TrustServerCertificate=no;Connection Timeout=30;"""
-        # params = urllib.parse.quote_plus(conn)
-        # conn_str = psycopg2.connect(conn)
-
         # return conn
         return conn
-
 
 app_settings = get_settings()
 CONN_STR = generate_db_string(
@@ -49,7 +39,7 @@ CONN_STR = generate_db_string(
 
 # check_same_thread = false only works in sqlite, not postgres or others
 if "sqlite" in CONN_STR:
-    # print("Using a sqlite database")
+    print("Using a sqlite database")
     connect_args = {"check_same_thread": False}
     engine = create_engine(CONN_STR, connect_args=connect_args)
 else:
